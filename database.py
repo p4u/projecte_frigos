7e8e39290276bd6530e3e379bb3f47eaaf059b63
p4u <pau@dabax.net>
2016-04-05 11:56:56 +0200

    Add loop to database connection. It blocks until mysql is up

--- database.py
+++ database.py
@@ -6,8 +6,16 @@ class database(object):
   def __init__(self):
     config = {}
     execfile(CONFIG_FILE,config)
-    self.db = MySQLdb.connect(config["host"],config["user"],config["password"],config["database"])
-    self.db.autocommit(True)
+    while not self._connect(config): print("Retrying database connection")
+
+  def _connect(self,config):
+    try:
+      self.db = MySQLdb.connect(config["host"],config["user"],config["password"],config["database"])
+      self.db.autocommit(True)
+      return True
+    except:
+      print("Cannot connect to MYSQL")
+      return False
 
   def insert(self,txt):
     dbc = self.db.cursor()
