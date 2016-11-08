7028a2ebfe1b968190f0de930bfd2e179820562e
p4u <pau@dabax.net>
2016-11-08 20:20:34 +0100

    Remove jsPDF local libraries but add remote CDN script from official source Fix export PDF/PNG sizes and add some description text Small cosmetic changes

--- web/static/js/export.js
+++ web/static/js/export.js
@@ -7,12 +7,21 @@ $(function() {
     html2canvas($("#placeholder").get(0), {
       onrendered: function(canvas) {
         document.body.appendChild(canvas);
-
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1;
+        var yyyy = today.getFullYear();
+        var date = dd+'/'+mm+'/'+yyyy;
         var imgData = canvas.toDataURL('image/png');
-        var doc = new jsPDF('landscape');
-
-        doc.addImage(imgData, 'PNG', 10, 10, 190, 95);
-        doc.save('sample-file.pdf');
+        var doc = new jsPDF('landscape','pt');
+        var w = 0;
+        var max = 800;
+        if (canvas.width > max) w = max;
+        else w = canvas.width;
+        var h = canvas.height * ( w/canvas.width );
+        doc.text('Gràfica per aparell '+devname+' a data '+date, 20, 20);
+        doc.addImage(imgData, 'PNG', 40, 60, w, h);
+        doc.save('captura-'+devname+'.pdf');
       }
     });
   });
